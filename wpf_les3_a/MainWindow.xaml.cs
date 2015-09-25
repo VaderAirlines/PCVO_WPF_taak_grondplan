c9aa264a153c35e3b641d824a1f005c7ca75b56d
VaderAirlines <dennisbellinkx@gmail.com>
2015-09-25 20:37:42 +0200

    test

---
+++ wpf_les3_a/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpf_les3_a.helper_classes;
+
+namespace wpf_les3_a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int allowedClosingMargin = 5;
+        PointCollection points = new PointCollection();
+        List<pcvoPolygon> polygons = new List<pcvoPolygon>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region UI handlers
+
+        private void canvas_clickHandler(object sender, MouseButtonEventArgs e)
+        {
+            Point position = e.GetPosition(canvas);
+
+            points.Add(new Point(position.X, position.Y));
+
+            if (points.Count > 1)
+            {
+                Point begin = points[0];
+                Point end = points[points.Count - 1];
+
+                if (withinRange(begin, end))
+                {
+                    PointCollection polygonPoints = points.Clone();
+                    polygonPoints.RemoveAt(points.Count - 1);
+                    createPolygon(polygonPoints);
+
+                    points.Clear();
+                    refreshCanvas(canvas);
+
+                    pcvoPolygon a = new pcvoPolygon();
+                   
+                }
+                else
+                {
+                    Line line = new Line();
+                    line.Visibility = System.Windows.Visibility.Visible;
+                    line.StrokeThickness = 4;
+                    line.Stroke = System.Windows.Media.Brushes.Black;
+                    line.X1 = points[points.Count - 2].X;
+                    line.X2 = points[points.Count - 1].X;
+                    line.Y1 = points[points.Count - 2].Y;
+                    line.Y2 = points[points.Count - 1].Y;
+                    canvas.Children.Add(line);
+                }
+            }
+        }
+
+        #endregion
+
+        #region helpers
+
+        private bool withinRange(Point begin, Point end)
+        {
+            if (((begin.X > end.X - allowedClosingMargin) && (begin.X < end.X + allowedClosingMargin))
+                && ((end.X > end.X - allowedClosingMargin) && (end.X < end.X + allowedClosingMargin)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void refreshCanvas(Canvas canvas)
+        {
+            canvas.Children.Clear();
+
+            foreach (pcvoPolygon p in polygons)
+            {
+                canvas.Children.Add(p.getPolygon());
+            }
+        }
+        
+        private void createPolygon(PointCollection points)
+        {
+            pcvoPolygon p = new pcvoPolygon();
+            p.points = points;
+
+            polygons.Add(p);
+            canvas.Children.Add(p.getPolygon());
+        }
+
+        #endregion
+    }
+}
---
+++ wpf_les3_a/helper classes/pcvoPolygon.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace wpf_les3_a.helper_classes {
+    public class pcvoPolygon {
+        //privates
+        private Polygon polygon;
+
+        //.ctor
+        public pcvoPolygon() {
+            this.polygon = new Polygon();
+            this.polygon.Stroke = Brushes.Black;
+            this.polygon.StrokeThickness = 2;
+            this.polygon.Fill = Brushes.Beige;
+
+            this.polygon.MouseEnter += mouseEnterHandler;
+            this.polygon.MouseLeave += mouseLeaveHandler;
+            this.polygon.MouseRightButtonUp += enablePolygonEditing;
+        }
+
+        //properties
+        public PointCollection points {
+            get { return this.polygon.Points; }
+            set { this.polygon.Points = value; }
+        }
+
+        public Polygon getPolygon() {
+            return this.polygon;
+        }
+
+        //private polygon handlers
+        private void mouseEnterHandler(object sender, System.Windows.Input.MouseEventArgs e) {
+            this.polygon.Fill = Brushes.AliceBlue;
+        }
+
+        private void mouseLeaveHandler(object sender, System.Windows.Input.MouseEventArgs e) {
+            this.polygon.Fill = Brushes.Beige;
+        }
+
+        private void enablePolygonEditing(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            MessageBox.Show("Right mouse click");
+        }
+
+    }
+
+
+}
